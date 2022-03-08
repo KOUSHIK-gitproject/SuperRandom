903498e1d1d36e79b734775fcc91f79792097e7a
KOUSHIK <Koushik.Senapati@yokogawa.com>
2022-03-08 08:31:12 +0530

    netget

---
+++ SuperRandom/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SuperRandom
+{
+
+        public class PersonalDetail
+        {
+            private string _firstname;
+            private string _lastname;
+            private string _Email;
+            private long _PhoneNumber;
+            public string FirstName
+            {
+                get { return _firstname; }
+                set { _firstname = value; }
+            }
+            public string LastName
+            {
+                get { return _lastname; }
+                set { _lastname = value; }
+            }
+            public string Email
+            {
+                get { return _Email; }
+                set { _Email = value; }
+            }
+            public long PhoneNumber
+            {
+                get { return _PhoneNumber; }
+                set { _PhoneNumber = value; }
+            }
+        }
+}
